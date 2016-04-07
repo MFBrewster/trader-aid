a421ccdc39c4eb2fe887f1a1541d83cc1634e8dc
mfbrewster <michael.f.brewster@gmail.com>
2016-04-06 20:09:12 -0400

    random commit before a deep dive into nowehereland

--- app/components/product-window/component.js
+++ app/components/product-window/component.js
@@ -4,20 +4,23 @@ export default Ember.Component.extend({
   tagName: 'div',
   classNames: ['product-half main-app-window'],
   newProduct: false,
-  // editProduct: false,
+  editProduct: false,
 
   product: {},
 
   actions: {
     toggleNewProd () {
       this.toggleProperty('newProduct');
+      this.set('editProduct', false);
     },
-    // toggleEdit(): {
-    //   this.toggleProperty('editProduct');
-    // }
+    toggleEdit () {
+      this.toggleProperty('editProduct');
+    },
     submit () {
-      this.sendAction('submit', this.get('product'));
-      this.set('product', {});
+      if (this.get('product.name') && this.get('product.price')) {
+        this.sendAction('submit', this.get('product'));
+        this.set('product', {});
+      }
       this.toggleProperty('newProduct');
     },
   }
